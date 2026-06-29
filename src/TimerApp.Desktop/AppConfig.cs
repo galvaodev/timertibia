62bc09716aec2463e5f8c04a7a90252eb7bc463a
galvaodev <galvaopp@gmail.com>
2026-06-29 13:03:08 -0300

    novo

--- src/TimerApp.Desktop/AppConfig.cs
+++ src/TimerApp.Desktop/AppConfig.cs
@@ -3,5 +3,5 @@ namespace TimerApp.Desktop;
 internal static class AppConfig
 {
     // Atualize com a URL do seu repositório GitHub antes de publicar
-    public const string GitHubRepoUrl = "https://github.com/Baconx/TibiaTimer";
+    public const string GitHubRepoUrl = "https://github.com/galvaodev/timertibia";
 }
